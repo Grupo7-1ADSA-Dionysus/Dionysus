53be2b3439c1e1e4fba7651dce87650618621259
Murakami-J <juliana.oshikawa@sptech.school>
2024-05-05 18:52:30 -0300

    Arrumando a tela de cadastro com validações e css - Juliana Murakami

--- site-institucional/cadastro/cadastro.js
+++ site-institucional/cadastro/cadastro.js
@@ -11,75 +11,85 @@ function cadastrar(){
     const TELEFONE = input_telefone.value;
     const SENHA = input_senha.value;
     const CONFIRMACAO_SENHA = input_confirmacaoSenha.value;
+    const TOKEN = input_token.value;
 
     span_avisoCampos.innerHTML = "";
 
-    if(NOME != "" && SOBRENOME != "" && EMAIL != "" && TELEFONE != "" && SENHA != "" && CONFIRMACAO_SENHA != ""){
+    if(NOME != "" && SOBRENOME != "" && EMAIL != "" && TELEFONE != "" && SENHA != "" && CONFIRMACAO_SENHA != "" && TOKEN != ""){
 
         // Email terá que ter um @ e um .
         const INDICE_ARROBA = EMAIL.indexOf("@");
         const INDICE_PONTO = EMAIL.indexOf(".");
 
         
-        span_avisoEmail.innerHTML = "";
-        span_avisoSenha.innerHTML = "";
-        
+        span_avisoEmail.style.color = 'transparent';
+        span_avisoSenha.style.color = 'transparent';
+        span_avisoConfirmacaoSenha.style.color = 'transparent';
+
         // Senha terá que ter pelo menos: 8 caracteres, 1 número, uma letra maiúscula 
         if(INDICE_ARROBA == -1 || INDICE_PONTO == -1){
             span_avisoEmail.innerHTML = "Email inválido";
-        }
-        
-        if(SENHA.length < 8){
-            span_avisoSenha.innerHTML = "A senha deve ter pelo menos 8 caracteres";
-
+            span_avisoEmail.style.color = 'red';
+            
         } else{
-            let existeNumero = false;
-
-            for(let numero = 0; numero <= 9; numero++){
-                const INDICE_NUMERO = SENHA.indexOf(`${numero}`);
+            if(SENHA.length < 8){
+                span_avisoSenha.innerHTML = "A senha deve ter pelo menos 8 caracteres";
+                span_avisoSenha.style.color = 'red';
     
-                if(INDICE_NUMERO != -1){
-                    existeNumero = true;
-
-                    numero = 10;
-                } 
-            }
-
-            if(existeNumero == true){
-                
-                let existeLetraMaiuscula = false;
-                let senha_replace = SENHA;
-
-                senha_replace = senha_replace.replace(/\d+/ig, "")
+            } else{
+                let existeNumero = false;
     
-                for(let posicao = 0; posicao < senha_replace.length; posicao++){
-                    const LETRA_MAIUSCULA = senha_replace[posicao];
+                for(let numero = 0; numero <= 9; numero++){
+                    const INDICE_NUMERO = SENHA.indexOf(`${numero}`);
+        
+                    if(INDICE_NUMERO != -1){
+                        existeNumero = true;
     
-                    if(LETRA_MAIUSCULA == LETRA_MAIUSCULA.toUpperCase()){
-                        posicao = senha_replace.length;
-                        existeLetraMaiuscula = true;
+                        numero = 10;
                     } 
                 }
     
-                if(existeLetraMaiuscula == false){
-                    span_avisoSenha.innerHTML = "A senha deve conter pelo menos uma letra maiúscula";
-
-                } else{
-                    span_avisoConfirmacaoSenha.innerHTML = "";
-
-                    if(SENHA != CONFIRMACAO_SENHA){
-                        
-                        span_avisoConfirmacaoSenha.innerHTML = "As senhas não conferem";
-
-                    } else{
-                        window.location.href = "../dashboard/index.html";
+                if(existeNumero == true){
+                    
+                    let existeLetraMaiuscula = false;
+                    let senha_replace = SENHA;
+    
+                    senha_replace = senha_replace.replace(/\d+/ig, "")
+        
+                    for(let posicao = 0; posicao < senha_replace.length; posicao++){
+                        const LETRA_MAIUSCULA = senha_replace[posicao];
+        
+                        if(LETRA_MAIUSCULA == LETRA_MAIUSCULA.toUpperCase()){
+                            posicao = senha_replace.length;
+                            existeLetraMaiuscula = true;
+                        } 
                     }
-                }
-            } else{
-                span_avisoSenha.innerHTML = "A senha deve conter pelo menos um número";
-            }   
-            
+        
+                    if(existeLetraMaiuscula == false){
+                        span_avisoSenha.innerHTML = "A senha deve conter uma letra maiúscula";
+                        span_avisoSenha.style.color = 'red';
+    
+                    } else{
+                        span_avisoConfirmacaoSenha.innerHTML = "";
+    
+                        if(SENHA != CONFIRMACAO_SENHA){
+                            
+                            span_avisoConfirmacaoSenha.innerHTML = "As senhas não conferem";
+                            span_avisoConfirmacaoSenha.style.color = 'red';
+    
+                        } else{
+                            window.location.href = "../dashboard/index.html";
+                        }
+                    }
+                } else{
+                    span_avisoSenha.innerHTML = "A senha deve conter pelo menos um número";
+                    span_avisoSenha.style.color = 'red';
+                }   
+                
+            }
         }
+        
+        
 
     } else{
         span_avisoCampos.innerHTML = "Preencha todos os campos"
