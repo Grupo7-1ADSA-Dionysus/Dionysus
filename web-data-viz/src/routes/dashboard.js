e834d4377ba01b372c1b61e8ec444860dbd53193
Murakami-J <juliana.oshikawa@sptech.school>
2024-06-14 11:57:36 -0300

    Adicionando cadastro de Hectare

--- web-data-viz/src/routes/dashboard.js
+++ web-data-viz/src/routes/dashboard.js
@@ -14,5 +14,15 @@ router.get("/tempo-real/:fkHectare", function (req, res) {
 router.get("/obterDadosGraficoMedia", function (req, res) {
     dashboardController.obterDadosGraficoMedia(req, res);
 })
+router.get("/exibirHectares/:nomePropriedade", function (req, res) {
+    dashboardController.exibirHectares(req, res);
+})
+router.get("/exibirAreaFazenda/:nomePropriedade", function (req, res) {
+    dashboardController.exibirAreaFazenda(req, res);
+})
+
+router.post("/cadastrarHectares/:idPropriedade", function (req, res) {
+    dashboardController.cadastrarHectares(req, res);
+})
 
 module.exports = router;
